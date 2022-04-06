73a2c12e53ad8be25b78e718b89c97db943281e6
moonjs0113 <littlemoom@naver.com>
2022-04-06 20:04:30 +0900

    [Feat] #20 : 하단의 플로팅 탭으로 다른 탭으로 이동할 수 있다.

--- Haro/Haro/View/MapView.swift
+++ Haro/Haro/View/MapView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import MapKit
+import CoreLocationUI
 
 struct IdentifiablePlace: Identifiable {
     let id: UUID
@@ -21,6 +22,7 @@ struct IdentifiablePlace: Identifiable {
 
 struct MapView: View {
     let place: IdentifiablePlace = IdentifiablePlace(lat: 36.014279, long: 129.325785)
+    
     @State var coordinateRegion: MKCoordinateRegion = MKCoordinateRegion (
         center: CLLocationCoordinate2D (
             latitude: 36.014279,
@@ -33,128 +35,46 @@ struct MapView: View {
     )
     
     var body: some View {
-        Map(coordinateRegion: self.$coordinateRegion, annotationItems: [self.place]) { place in
-            MapPin(coordinate: self.place.location, tint: Color.purple)
+        ZStack {
+            Map(coordinateRegion: self.$coordinateRegion, annotationItems: [self.place] ) { place in
+                MapPin(coordinate: self.place.location, tint: Color.purple)
+            }
+            CreateStoryButton()
         }
         .ignoresSafeArea()
     }
 }
 
-struct MainView: View {
-    @State var currentPageIndex: Int = 2
-    var body: some View {
-        ZStack {
-            PageControlView(currentPageIndex: self.currentPageIndex)
-                .ignoresSafeArea()
-            FloatingTabView(currentPageIndex: self.$currentPageIndex)
-        }
-    }
-}
 
-struct PageControlView: View {
-    var currentPageIndex: Int
-    var body: some View {
-        GeometryReader { geometry in
-            ScrollViewReader { scrollProxy in
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack {
-                        MapView()
-                            .frame(width: geometry.size.width, height: geometry.size.height)
-                            .id(0)
-                        CommunityView()
-                            .frame(width: geometry.size.width, height: geometry.size.height)
-                            .id(1)
-                        MyPageView()
-                            .frame(width: geometry.size.width, height: geometry.size.height)
-                            .id(2)
-                    }
-                    .onChange(of: self.currentPageIndex) { target in
-                        withAnimation{
-                            scrollProxy.scrollTo(target)
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-
-struct FloatingTabView: View {
-    let cornerRadius: CGFloat = 17
-    
-    @Binding var currentPageIndex: Int
-    
-    func floatingTabButton(imageName: String, title: String, pageIndex: Int) -> some View{
-        return Button {
-            withAnimation {
-                self.currentPageIndex = pageIndex
-                print("Change Scroll Proxy")
-            }
-        } label: {
-            let isSelected = (self.currentPageIndex == pageIndex)
-            FloatingTabButtonView(imageName: imageName, title: title, isSelected: isSelected)
-        }
-        
-    }
-    
-    var body: some View {
-        VStack(alignment: .center) {
-            Spacer()
-            ZStack {
-                RoundedRectangle(cornerRadius: self.cornerRadius, style: .circular)
-                    .fill(.white)
-                    .frame(height: 80)
-                    .shadow(color: .gray, radius: 1, x: 0, y: 0.5)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 17)
-                            .stroke(.gray,lineWidth: 1)
-                    )
-                HStack(alignment: .center) {
-                    Spacer()
-                    Spacer()
-                    Spacer()
-                    HStack {
-                        self.floatingTabButton(imageName: "map", title: "지도", pageIndex: 0)
-                        Spacer()
-                        self.floatingTabButton(imageName: "person", title: "커뮤니티", pageIndex: 1)
-                        Spacer()
-                        self.floatingTabButton(imageName: "person", title: "마이페이지", pageIndex: 2)
-                    }
-                    Spacer()
-                    Spacer()
-                    Spacer()
-                }
-            }
-        }
-        .padding([.leading, .trailing], 12)
-    }
-}
-
-struct FloatingTabButtonView: View {
-    let imageName: String
-    let title: String
-    let isSelected: Bool
-    
-    let deselectedColor: Color = Color.gray.opacity(0.8)
-    
+struct CreateStoryButton: View {
     var body: some View {
         VStack {
-            Image(systemName: self.imageName)
-                .imageScale(.large)
-                .blendMode(.plusDarker)
-                .foregroundColor(self.isSelected ? Color.black : self.deselectedColor)
-            Text(self.title)
-                .lineLimit(1)
-                .font(.system(size: 12, weight: self.isSelected ? .bold : .regular))
-                .foregroundColor(self.isSelected ? Color.black : self.deselectedColor)
-                .frame(minHeight: 15)
+            Spacer()
+            Button {
+                print("아무일도 일어나지 않습니다.")
+            } label: {
+                ZStack {
+                    Capsule()
+                        .fill(Color.init(red: 1, green: 144/255, blue: 0))
+                    Text("지금 어떤 일이 일어나고 있나요?")
+                        .lineLimit(1)
+                        .font(.system(size: 15))
+                        .minimumScaleFactor(0.005)
+                        .foregroundColor(.white)
+                        .padding([.leading, .trailing], 30)
+                }
+            }
+            .frame(height: 50)
+            .padding([.leading, .trailing], 90)
+            
+            Spacer()
+                .frame(height: 130)
         }
-        .frame(width: 75, height: 75)
     }
 }
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MainView()
+        MapView()
     }
 }
