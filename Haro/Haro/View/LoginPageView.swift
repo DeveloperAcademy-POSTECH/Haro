7be7c336b1acc67ea64d450176ec22ccc647ed82
kelly1422 <kelly1422@naver.com>
2022-04-10 22:32:50 +0900

    [Feat] #64 : 회원가입 페이지 UI 작성.

--- Haro/Haro/View/LoginPageView.swift
+++ Haro/Haro/View/LoginPageView.swift
@@ -7,8 +7,6 @@
 
 import SwiftUI
 
-
-
 struct LoginPageView: View {
     let screenHeight = UIScreen.main.bounds.size.height
     let screenWidth = UIScreen.main.bounds.size.width
@@ -66,8 +64,8 @@ struct LoginPageView: View {
                             .padding(.top, 30)
                     }
                     
-                    Button {
-                        Void()
+                    NavigationLink {
+                        SignUpPageView()
                     } label: {
                         Text("회원가입")
                             .font(.headline)
@@ -85,6 +83,113 @@ struct LoginPageView: View {
     }
 }
 
+struct SignUpPageView: View {
+    let screenHeight = UIScreen.main.bounds.size.height
+    let screenWidth = UIScreen.main.bounds.size.width
+    
+    @State var name = ""
+    @State var email = ""
+    @State var pwd = ""
+    @State var pwdConfirm = ""
+    
+    var body: some View {
+        NavigationView{
+            VStack{
+                HStack{
+                    Image(systemName: "person")
+                        .font(.title2)
+                        .padding(.trailing, 20)
+                    
+                    TextField("이름", text: $name)
+                        .autocapitalization(.none)
+                        .disableAutocorrection(true)
+                        .padding()
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 20)
+                                .stroke(lineWidth: 1)
+                                .foregroundColor(Color.init(red: 255/255, green: 180/255, blue: 10/255))
+                        )
+                }
+                .padding(.horizontal, 35)
+                .padding(.vertical, 10)
+               
+                HStack{
+                    Image(systemName: "envelope")
+                        .font(.title2)
+                        .padding(.trailing, 20)
+                    
+                    TextField("이메일", text: $email)
+                        .autocapitalization(.none)
+                        .disableAutocorrection(true)
+                        .padding()
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 20)
+                                .stroke(lineWidth: 1)
+                                .foregroundColor(Color.init(red: 255/255, green: 180/255, blue: 10/255))
+                        )
+                }
+                .padding(.horizontal, 35)
+                .padding(.vertical, 10)
+                
+                HStack{
+                    Image(systemName: "lock")
+                        .font(.title2)
+                        .padding(.trailing, 20)
+                    
+                    SecureField("비밀번호", text: $pwd)
+                        .autocapitalization(.none)
+                        .disableAutocorrection(true)
+                        .padding()
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 20)
+                                .stroke(lineWidth: 1)
+                                .foregroundColor(Color.init(red: 255/255, green: 180/255, blue: 10/255))
+                        )
+                }
+                .padding(.horizontal, 35)
+                .padding(.vertical, 10)
+                
+                HStack{
+                    Image(systemName: "lock.shield")
+                        .font(.title2)
+                        .padding(.trailing, 20)
+                    
+                    SecureField("비밀번호 확인", text: $pwdConfirm)
+                        .autocapitalization(.none)
+                        .disableAutocorrection(true)
+                        .padding()
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 20)
+                                .stroke(lineWidth: 1)
+                                .foregroundColor(Color.init(red: 255/255, green: 180/255, blue: 10/255))
+                        )
+                }
+                .padding(.horizontal, 35)
+                .padding(.vertical, 10)
+                
+                Spacer()
+                
+                Button {
+                    guard !email.isEmpty, !pwd.isEmpty else {
+                        return
+                    }
+                    print("login")
+                } label: {
+                    Text("회원가입")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 180, height: 50)
+                        .background(Color.init(red: 255/255, green: 180/255, blue: 10/255))
+                        .cornerRadius(80)
+                        .padding(.top, 20)
+                }
+
+                Spacer()
+            }
+        }
+    }
+}
 
 struct LoginPageView_Previews: PreviewProvider {
     static var previews: some View {
